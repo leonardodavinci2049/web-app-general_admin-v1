ca73fb26f73fdb0346c4788a04fc51d89aa04b90
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-01-13 14:15:54 -0300

    feat: implement getAllOrganizations function and update organization page to use it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
-import { getOrganizations } from "@/server/organizations";
+import { getAllOrganizations, getOrganizations } from "@/server/organizations";
 import { SiteHeaderWithBreadcrumb } from "./_components/header/site-header-with-breadcrumb";
 import { CreateOrganizationDialog } from "./admin/_components/create-organization-dialog";
 import { OrganizationTable } from "./admin/_components/organization-table";
+import { connection } from "next/server";
 
 export default async function DashboardPage() {
-  const organizations = await getOrganizations();
+  await connection();
+  const organizations = await getAllOrganizations();
   return (
     <div className="flex h-screen flex-col bg-background">
       {/* Header fixo no topo */}
@@ -20,7 +22,7 @@ export default async function DashboardPage() {
       <div className="container mx-auto py-10 px-4 space-y-6">
         <div className="flex items-center justify-between">
           <div>
-            <h1 className="text-3xl font-bold tracking-tight">Empresas</h1>
+            <h1 className="text-3xl font-bold tracking-tight">Organizações</h1>
             <p className="text-muted-foreground">
               Gerencie suas Empresas Clientes.
             </p>
