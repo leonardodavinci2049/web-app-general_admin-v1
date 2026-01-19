4e98617bb5626146b511f8f6a7e4b6e2c0a57490
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-01-19 18:51:58 -0300

    chore: Cleanliness in the project

--- src/services/api-assets/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Assets API Service - Main export
- */
-
-export type * from "@/services/api-assets/types/api-assets";
-export { AssetsApiService, assetsApiService } from "./assets-api-service";
