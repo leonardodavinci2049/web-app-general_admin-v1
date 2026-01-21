8b21ec0bd500376840b24effcc30fa6a9acdd3a4
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-01-21 15:59:56 -0300

    fix: error lint

--- src/app/dashboard/users/_components/user-actions.tsx
+++ src/app/dashboard/users/_components/user-actions.tsx
@@ -7,7 +7,6 @@ import {
   ShieldAlert,
   UserCheck,
   UserCog,
-  UserMinus,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -21,7 +20,6 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import {
