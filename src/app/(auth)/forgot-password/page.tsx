8936076beb71c3adcf5796ec494e7449438bb46a
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-03-01 09:12:26 -0300

    chore: folder auth changed

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,26 +1,37 @@
+import type { Metadata } from "next";
 import Image from "next/image";
-import Link from "next/link";
-import { ForgotPasswordForm } from "./components/forgot-password-form";
+import { Card } from "@/components/ui/card";
+import ForgotPasswordForm from "./components/ForgotPasswordForm";
+
+export const metadata: Metadata = {
+  title: "Forgot password",
+};
 
 export default function ForgotPasswordPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link
-          className="flex items-center gap-2 self-center font-medium"
-          href="/"
-        >
-          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <Image
-              alt="Better Auth Starter Logo"
-              height={50}
-              priority
-              src={"/better-auth-starter.png"}
-              width={50}
-            />
+    <div className="flex items-center justify-center p-4">
+      {/* Card para telas grandes com formulário e imagem */}
+      <Card className="bg-card/50 hidden w-full max-w-5xl overflow-hidden border-0 shadow-2xl backdrop-blur-sm lg:grid lg:h-[600px] lg:grid-cols-2">
+        <div className="flex flex-col gap-4 p-6 md:p-10">
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full max-w-sm">
+              <ForgotPasswordForm />
+            </div>
           </div>
-          Better Auth Starter
-        </Link>
+        </div>
+        <div className="relative flex items-center justify-center">
+          <Image
+            src="/images/auth/logo-winerp-banner-auth.png"
+            alt="Dashboard Background"
+            fill
+            className="object-contain object-center"
+            priority
+          />
+        </div>
+      </Card>
+
+      {/* Formulário simples para telas menores */}
+      <div className="w-full max-w-sm lg:hidden">
         <ForgotPasswordForm />
       </div>
     </div>
