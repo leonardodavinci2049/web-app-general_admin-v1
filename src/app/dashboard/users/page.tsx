001d9de79f345c55b8e926791ff60ae6425b2fba
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-06-02 20:53:03 -0300

    refactor: reorder import statements in users dashboard page

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -4,9 +4,9 @@ import { connection } from "next/server";
 import { auth } from "@/lib/auth/auth";
 import { getUsersWithOrganizations } from "@/services/user/user-cached-service";
 import { SiteHeaderWithBreadcrumb } from "../_components/header/site-header-with-breadcrumb";
+import { CreateUserDialog } from "./_components/create-user-dialog";
 import { UserSearch } from "./_components/user-search";
 import { UserTable } from "./_components/user-table";
-import { CreateUserDialog } from "./_components/create-user-dialog";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
